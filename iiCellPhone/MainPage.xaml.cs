5d4f31d74e539e556ed88c833bf7519ccb9d8974
Isaac Ingles <isaac.ingles@gmail.com>
2020-11-08 17:03:22 -0700

    Add project files.

---
+++ iiCellPhone/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace iiCellPhone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///  // When the button is clicked, display the results
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSubmit_Clicked(object sender, EventArgs e)
+        {
+            // Instantiate an object of the Phone class
+            Phone cellPhone = new Phone();
+
+
+            // If brand or model is an empty string, input isn't valid
+            if (EntryBrand.Text != "" && EntryModel.Text != "")
+            {
+                cellPhone.Brand = EntryBrand.Text;
+                cellPhone.Model = EntryModel.Text;
+
+                if (ValidPrice(cellPhone))
+                {
+                    cellPhone.PhoneMarkup();
+                    DisplayPhoneDetails(cellPhone);
+                }
+            }
+
+            else
+            {
+                DisplayAlert("Enter a string", "Please enter a value for the brand and model", "Close");
+            }
+
+        }
+
+        /// <summary>
+        ///  Display the results
+        /// </summary>
+        /// <param name="cellPhone"></param>
+        private void DisplayPhoneDetails(Phone cellPhone)
+        {
+            // use a template string to set the label text with price to currency string
+            LblResults.Text = $"Brand: {cellPhone.Brand}\nModel:{cellPhone.Model}\nPrice: {cellPhone.Price.ToString("c")}";
+        }
+
+        /// <summary>
+        ///  Check to see if the price is valid
+        /// </summary>
+        /// <param name="cellPhone"></param>
+        /// <returns></returns>
+        public bool ValidPrice(Phone cellPhone)
+        {
+
+            if (decimal.TryParse(EntryPrice.Text, out decimal price) && price >=0)
+            {
+                // Set the price
+                cellPhone.Price = price;
+                return true;
+            }
+            else
+            {
+                // if it isn't valid, display an alert
+                DisplayAlert("Invalid Input", "Please enter a number for the price", "Close");
+                return false;
+            }
+        }
+    }
+}
---
+++ iiCellPhone/Phone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iiCellPhone
+{
+    public class Phone
+    {
+        private string _brand;
+        private string _model;
+        private decimal _price;
+
+        public int age { get; set; }
+
+        // Constructor
+        public Phone()
+        {
+            // Set default values
+            _brand = "";
+            _model = "";
+            _price = 0;
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public void PhoneMarkup()
+        {
+            MarkupPhonePrice();
+        }
+
+        private void MarkupPhonePrice()
+        {
+            if (_model == "iPhone")
+            {
+                // Mark price up 1.56 percent
+                _price *= 1.56m;
+            }
+            else
+            {                   
+                _price *= 2.56m;
+            }
+        }
+    }
+}
